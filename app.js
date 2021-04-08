29f71e74f82080340063a1a61ccb651aa1580bc7
onlysoa <only.soa@gmail.com>
2021-04-08 12:50:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
-app.get('/', (req, res) => res.send('Hello World. welcome to CICD Pipleline on AWS');
+app.get('/', (req, res) => res.send('Hello World. Welcome to CICD Pipleline on AWS');
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
